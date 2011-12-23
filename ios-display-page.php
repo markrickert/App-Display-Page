3987dddceec3f1cfb354da43eae22aaa61096d75
Mark Rickert <mjar81@gmail.com>
2011-12-23 11:05:23 -0500

    Added image caching.

--- ios-display-page.php
+++ ios-display-page.php
@@ -12,13 +12,14 @@ define('IOS_APP_PAGE_APPSTORE_URL', 'http://ax.itunes.apple.com/WebObjects/MZSto
 define('IOS_APP_PAGE_ICON_SIZE', 175);
 define('IOS_APP_PAGE_IMAGE_SIZE', 120);
 define('IOS_APP_PAGE_CACHE_TIME', 60 * 60 * 24); //One Day
+define('IOS_APP_PAGE_CACHE_IMAGES', true);
 
 add_shortcode('ios-app', 'ios_display_page_shortcode');
 add_shortcode('ios_app', 'ios_display_page_shortcode');
 add_action('wp_print_styles', 'ios_display_page_add_stylesheet');
 
 //Available Actions
-$actions = array('name', 'version', 'price', 'release_notes', 'description', 'rating', 'iphoness', 'ipadss', 'itunes_link');
+$actions = array('name', 'icon', 'icon_url', 'version', 'price', 'release_notes', 'description', 'rating', 'iphoness', 'ipadss', 'itunes_link');
 foreach ($actions as $action) {
 	add_shortcode('ios_app_' . $action, 'ios_app_' . $action);
 }
@@ -27,6 +28,24 @@ function ios_app_name( $atts ) {
 	$app = ios_app_get_data(ios_ap_extract_id($atts));
 	return $app->trackName;
 }
+
+function ios_app_icon( $atts ) {
+	return '<img src="' . ios_app_icon_url($atts) . '" />';
+}
+
+function ios_app_icon_url( $atts ) {
+	$app = ios_app_get_data(ios_ap_extract_id($atts));
+
+	$artwork_url = $app->artworkUrl100;
+	if(IOS_APP_PAGE_CACHE_IMAGES == true)
+	{
+		$upload_dir = wp_upload_dir();
+		$artwork_url = $upload_dir['baseurl'] . '/ios-app/' . $app->trackId . '/' . basename($app->artworkUrl100);
+	}
+
+	return $artwork_url;
+}
+
 
 function ios_app_version( $atts ) {
 	$app = ios_app_get_data(ios_ap_extract_id($atts));
@@ -61,6 +80,11 @@ function ios_app_iphoness( $atts ) {
 	$retval = '<ul>';
 	foreach($app->screenshotUrls as $ssurl) {
 		$ssurl = str_replace(".png", ".320x480-75.jpg", $ssurl);
+		if(IOS_APP_PAGE_CACHE_IMAGES == true)
+		{
+			$upload_dir = wp_upload_dir();
+			$ssurl = $upload_dir['baseurl'] . '/ios-app/' . $app->trackId . '/' . basename($ssurl);
+		}
 		$retval .= '<li class="app-screenshot"><a href="' . $ssurl . '" alt="Full Size Screenshot"><img src="' . $ssurl . '" width="' . IOS_APP_PAGE_IMAGE_SIZE . '" /></a></li>';
 	}
 	$retval .= '</ul>';
@@ -71,7 +95,11 @@ function ios_app_ipadss( $atts ) {
 	$app = ios_app_get_data(ios_ap_extract_id($atts));
 	$retval = '<ul>';
 	foreach($app->ipadScreenshotUrls as $ssurl) {
-		$ssurl = str_replace(".png", ".320x480-75.jpg", $ssurl);
+		if(IOS_APP_PAGE_CACHE_IMAGES == true)
+		{
+			$upload_dir = wp_upload_dir();
+			$ssurl = $upload_dir['baseurl'] . '/ios-app/' . $app->trackId . '/' . basename($ssurl);
+		}
 		$retval .= '<li class="app-screenshot"><a href="' . $ssurl . '" alt="Full Size Screenshot"><img src="' . $ssurl . '" width="' . IOS_APP_PAGE_IMAGE_SIZE . '" /></a></li>';
 	}
 	$retval .= '</ul>';
@@ -105,11 +133,12 @@ function ios_app_get_data( $id ) {
 	$ios_app_options = get_option('ios-app-' . $id, '');
 		
 	if($ios_app_options == '' || $ios_app_options['next_check'] < time()) {
-	
+		
 		$ios_app_options_data = ios_display_page_get_json($id);
 		$ios_app_options = array('next_check' => time() + IOS_APP_PAGE_CACHE_TIME, 'app_data' => $ios_app_options_data);
 
 		update_option('ios-app-' . $id, $ios_app_options);
+		if(IOS_APP_PAGE_CACHE_IMAGES == true)ios_app_save_images_locally($ios_app_options['app_data']);
 	}
 	
 	return $ios_app_options['app_data'];
@@ -138,19 +167,36 @@ function ios_display_page_get_json($id) {
 }
 
 function ios_display_page_get_json_via_fopen($id) {
-	return json_decode(file_get_contents(IOS_APP_PAGE_APPSTORE_URL . $id));
+	return json_decode(ios_app_fopenme(IOS_APP_PAGE_APPSTORE_URL . $id));
 }
 
 function ios_display_page_get_json_via_curl($id) {
+	return json_decode(ios_app_curlme(IOS_APP_PAGE_APPSTORE_URL . $id));
+}
+
+function ios_app_fopenme ($url) {
+	return file_get_contents($url);
+}
+
+function ios_app_curlme ($url) {
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, IOS_APP_PAGE_APPSTORE_URL . $id);
+    curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_TIMEOUT, 10);
     $output = curl_exec($ch);
     curl_close($ch);
+	return $output;
+}
 
-	return json_decode($output);
+function ios_app_fopen_or_curl($url)
+{
+	if(function_exists('file_get_contents') && ini_get('allow_url_fopen'))
+		return ios_app_fopenme($url);
+	else if(function_exists('curl_exec'))
+		return ios_app_curlme($url);
+	else
+		wp_die('<h1>You must have either file_get_contents() or curl_exec() enabled on your web server. Please contact your hosting provider.</h1>');
 }
 
 
@@ -158,7 +204,17 @@ function ios_display_page_output($app, $download_url) {
 ?>
 <div class="app-wrapper">
 
-	<img class="app-icon" src="<?php echo $app->artworkUrl100; ?>" width="<?php echo IOS_APP_PAGE_ICON_SIZE; ?>" height="<?php echo IOS_APP_PAGE_ICON_SIZE; ?>" />
+	<?php 
+ 		$artwork_url = $app->artworkUrl100;
+		if(IOS_APP_PAGE_CACHE_IMAGES == true)
+		{
+			$upload_dir = wp_upload_dir();
+			$artwork_url = $upload_dir['baseurl'] . '/ios-app/' . $app->trackId . '/' . basename($app->artworkUrl100);
+		}
+
+ 	?>
+	
+	<img class="app-icon" src="<?php echo $artwork_url; ?>" width="<?php echo IOS_APP_PAGE_ICON_SIZE; ?>" height="<?php echo IOS_APP_PAGE_ICON_SIZE; ?>" />
 	
 	<h1 class="app-title"><?php echo $app->trackName; ?><span class="app-version"> <?php echo $app->version; ?></span></h1>
 
@@ -195,6 +251,13 @@ function ios_display_page_output($app, $download_url) {
 		<?php
 		foreach($app->screenshotUrls as $ssurl) {
 			$ssurl = str_replace(".png", ".320x480-75.jpg", $ssurl);
+
+			if(IOS_APP_PAGE_CACHE_IMAGES == true)
+			{
+				$upload_dir = wp_upload_dir();
+				$ssurl = $upload_dir['baseurl'] . '/ios-app/' . $app->trackId . '/' . basename($ssurl);
+			}
+
 			echo '<li class="app-screenshot"><a href="' . $ssurl . '" alt="Full Size Screenshot"><img src="' . $ssurl . '" width="' . IOS_APP_PAGE_IMAGE_SIZE . '" /></a></li>';
 		}
 		?>
@@ -208,6 +271,12 @@ function ios_display_page_output($app, $download_url) {
 		<ul class="app-screenshots">
 		<?php
 		foreach($app->ipadScreenshotUrls as $ssurl) {
+			if(IOS_APP_PAGE_CACHE_IMAGES == true)
+			{
+				$upload_dir = wp_upload_dir();
+				$ssurl = $upload_dir['baseurl'] . '/ios-app/' . $app->trackId . '/' . basename($ssurl);
+			}
+			
 			echo '<li class="app-screenshot"><a href="' . $ssurl . '" alt="Full Size Screenshot"><img src="' . $ssurl . '" width="' . IOS_APP_PAGE_IMAGE_SIZE . '" /></a></li>';
 		}
 		?>
@@ -216,10 +285,60 @@ function ios_display_page_output($app, $download_url) {
 	<?php }
 }
 
-function ios_ap_extract_id( $atts )
-{
+function ios_ap_extract_id( $atts ) {
 	extract( shortcode_atts( array(
 		'id' => ''
 	), $atts ) );
 	return $id;
+}
+
+function ios_app_save_images_locally($app) {
+	$upload_dir = wp_upload_dir();
+	
+	if(!is_writeable($upload_dir['basedir'])) {
+		echo 'upload dir is not writeable';
+		define('IOS_APP_PAGE_CACHE_IMAGES', false);
+		return;
+	} else {
+		//Loop through screenshots and the app icons and cache everything
+		if(!is_dir($upload_dir['basedir'] . '/ios-app/' . $app->trackId)) {
+			if(!mkdir($upload_dir['basedir'] . '/ios-app/' . $app->trackId, 0755, true))
+			{
+				define('IOS_APP_PAGE_CACHE_IMAGES', false);
+				return;	
+			}
+		}
+
+		$urls_to_cache = array();
+		
+		$urls_to_cache[] = $app->artworkUrl60;
+		$urls_to_cache[] = $app->artworkUrl100;
+		$urls_to_cache[] = $app->artworkUrl512;
+
+		foreach($app->screenshotUrls as $ssurl) {
+			$ssurl2 = str_replace(".png", ".320x480-75.jpg", $ssurl);
+			$urls_to_cache[] = $ssurl;
+			$urls_to_cache[] = $ssurl2;
+		}
+		foreach($app->ipadScreenshotUrls as $ssurl) {
+			$ssurl2 = str_replace(".png", ".320x480-75.jpg", $ssurl);
+			$urls_to_cache[] = $ssurl;
+			$urls_to_cache[] = $ssurl2;
+		}
+
+		foreach($urls_to_cache as $url) {
+			$content = ios_app_fopen_or_curl($url);
+			
+			if($fp = fopen($upload_dir['basedir'] . '/ios-app/' . $app->trackId . '/' . basename($url), "w+"))
+			{
+				fwrite($fp, $content);
+				fclose($fp);
+			}
+			else {
+				//Couldnt write the file. Permissions must be wrong.
+				define('IOS_APP_PAGE_CACHE_IMAGES', false);
+				return;
+			}
+		}
+	}
 }
